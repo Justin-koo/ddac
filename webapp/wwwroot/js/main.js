0831f78007e0ac6da0ef46812e538c49586098b1
Justin <justinkwy6246@gmail.com>
2024-07-18 02:26:41 +0800

    css

--- webapp/wwwroot/js/main.js
+++ webapp/wwwroot/js/main.js
@@ -17,22 +17,22 @@ for(var i = 0; i < sidebarItems.length; i++) {
 
 window.addEventListener('DOMContentLoaded', (event) => {
     var w = window.innerWidth;
-    if(w < 1200) {
+    if(w < 992) {
         document.getElementById('sidebar').classList.remove('active');
     }
 });
 window.addEventListener('resize', (event) => {
     var w = window.innerWidth;
-    if(w < 1200) {
+    if(w < 992) {
         document.getElementById('sidebar').classList.remove('active');
     }else{
         document.getElementById('sidebar').classList.add('active');
     }
 });
 
-document.querySelector('.burger-btn').addEventListener('click', () => {
-    document.getElementById('sidebar').classList.toggle('active');
-})
+//document.querySelector('.burger-btn').addEventListener('click', () => {
+//    document.getElementById('sidebar').classList.toggle('active');
+//})
 document.querySelector('.sidebar-hide').addEventListener('click', () => {
     document.getElementById('sidebar').classList.toggle('active');
 
