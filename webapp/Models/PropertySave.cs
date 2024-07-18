1d7cf9c2a59eb0a276517f6252e77f340f9aaa21
fooarthur7 <116236948+fooarthur7@users.noreply.github.com>
2024-07-18 12:07:10 +0800

    Search function

---
+++ webapp/Models/PropertySave.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+using webapp.Areas.Identity.Data;
+
+namespace webapp.Models
+{
+	public class PropertySave
+	{
+		[Key]
+		public int Id { get; set; }
+		
+		public String UserId { get; set; }
+
+		[ForeignKey("UserId")]
+		public webappUser user { get; set; }
+
+		public int PropertyId { get; set; }
+
+	}
+}
